4bff87752ab5fb1be5c8e3c6bf3ee33ccb193488
Mariah Birgen <mariah.birgen@wartburg.edu>
2021-02-24 07:36:59 -0600

    02-23-2021 Data

--- FixSummary.R
+++ FixSummary.R
@@ -6,19 +6,19 @@ date = as.character(mdy(temp1[1]))
 
 Summary <- str_remove_all(Summary, " Translate") %>% 
     str_remove_all("Translate")
-CountyData <- Summary[44:149] %>%
+CountyDataOld <- Summary[44:149] %>%
     strsplit("(?= [A-Za-z])(?<=[0-9])|(?= [0-9])(?<=[A-Za-z])", perl=TRUE)
-CountyData <- CountyData[-c(10:12,72:74)]
-CountyData <- plyr::ldply(CountyData)
-temp <- strsplit(CountyData[,2], " ", fixed = TRUE)
+CountyDataOld <- CountyDataOld[-c(10:12,72:74)]
+CountyDataOld <- plyr::ldply(CountyDataOld)
+temp <- strsplit(CountyDataOld[,2], " ", fixed = TRUE)
 temp <- plyr::ldply(temp)
-temp[,1] = CountyData[,1]
+temp[,1] = CountyDataOld[,1]
 colnames(temp) = c("County", "Individuals.Tested","Individuals.Positive", 
                    "Total.Recovered", "Total.Deaths")
-CountyData <- temp
+CountyDataOld <- temp
 rm(temp)
-CountyData[-1] = sapply(CountyData[-1], as.integer)
-CountyData <- as.data.frame(CountyData) %>%
+CountyDataOld[-1] = sapply(CountyDataOld[-1], as.integer)
+CountyDataOld <- as.data.frame(CountyDataOld) %>%
     arrange(County)%>%
     mutate(Active = Individuals.Positive - Total.Recovered - Total.Deaths,
            positivity = Individuals.Positive/Individuals.Tested,
@@ -26,11 +26,11 @@ CountyData <- as.data.frame(CountyData) %>%
     )
 
 temp <-c(County = "Totals",
-          colSums(CountyData[,2:6]),
+          colSums(CountyDataOld[,2:6]),
           positivity = NA,
           date = as.character(mydate))
 temp["positivity"] = as.integer(temp["Individuals.Positive"])/as.integer(temp["Individuals.Tested"])
-CountyData[101,] = temp
+CountyDataOld[101,] = temp
 
 ###############################################
 # Remove other interesting data
@@ -52,3 +52,15 @@ colnames(SummaryData) = names(temp)
 temp <- rbind(temp, SummaryData) 
 write.csv(temp, file = "SummaryData.csv", row.names = FALSE)
 rm(temp, temp1)
+
+###################################################
+d_county <- t(CountyData[,-1])
+county_names <- d_county[1,]
+colnames(d_county) <- d_county[1,]
+d_county = d_county[-1,]
+# colnames(d_county)[1] <- "date"
+d_county[-9,] = sapply(d_county[-9,], as.integer)
+d_county[9,] = sapply(d_county[9,], as.numeric)
+# d_county <- as.data.frame(d_county)%>%
+#     mutate(Total = Reduce(`+`, .))
+d_county <- cbind(date=as.character(mydate),d_county)
